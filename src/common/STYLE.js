9c990578079d65c7de9ee396f16cbb23292f0241
Adrian Li <li.adrianmc@gmail.com>
2017-12-30 02:58:45 -0500

    add debugging shadow

--- src/common/STYLE.js
+++ src/common/STYLE.js
@@ -6,7 +6,8 @@ export const COLORS = {
 
 export const SHADOWS = {
   light: `0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)`,
-  dark: `0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23)`
+  dark: `0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)`,
+  debugRed: `0 0px 50px rgba(255, 0, 0, 0.5), 0 0px 50px rgba(255, 0, 0, 0.8)`
 };
 
 export const DIMENSIONS = {
