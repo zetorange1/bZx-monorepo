12af75a60c592de4f3c4dc6dbc0a19e7b464b4a2
William <zygroup1@gmail.com>
2018-01-08 02:06:56 -0500

    Monitor script updates.

--- restart_blockchain.sh
+++ restart_blockchain.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+## to run at boot, add this to /etc/rc.local -> cd /path/to/here && su ec2-user -c ./restart_blockchain.sh
+
 rimraf ./build/contracts/*
 truffle compile
-npm run stop_blockchain 
+npm run stop_blockchain
 npm run deploy_network
 npm run start_blockchain
 sleep 3
