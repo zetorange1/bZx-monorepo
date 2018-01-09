53943202aea45238ab0e33fe10f3e715961f2b9c
Adrian Li <li.adrianmc@gmail.com>
2018-01-09 08:50:40 -0500

    complete tooltips for margin amounts

--- src/orders/GenerateOrder/MarginAmounts.js
+++ src/orders/GenerateOrder/MarginAmounts.js
@@ -1,9 +1,20 @@
 import styled from "styled-components";
-import MuiTextField from "material-ui/TextField";
+import Input, { InputLabel, InputAdornment } from "material-ui/Input";
+import {
+  FormControl as MuiFormControl,
+  FormHelperText
+} from "material-ui/Form";
+import Tooltip from "material-ui/Tooltip";
+
 import Section, { SectionLabel } from "../../common/FormSection";
 
-const TextField = styled(MuiTextField)`
+const FormControl = styled(MuiFormControl)`
   margin: 24px !important;
+`;
+
+const ToolTipHint = styled.span`
+  text-decoration: underline;
+  cursor: pointer;
 `;
 
 export default ({
@@ -14,27 +25,40 @@ export default ({
   <Section>
     <SectionLabel>Margin Amounts</SectionLabel>
     <div style={{ textAlign: `center` }}>
-      <TextField
-        type="number"
-        label="Initial margin amount"
-        value={initialMarginAmount}
-        onChange={setStateForInput(`initialMarginAmount`)}
-        margin="normal"
-        required
-      />
-      <TextField
-        type="number"
-        label="Liquidation margin amount"
-        value={liquidationMarginAmount}
-        onChange={setStateForInput(`liquidationMarginAmount`)}
-        margin="normal"
-        required
-      />
+      <FormControl>
+        <InputLabel>Initial Margin Amount *</InputLabel>
+        <Input
+          value={initialMarginAmount}
+          type="number"
+          onChange={setStateForInput(`initialMarginAmount`)}
+          endAdornment={<InputAdornment position="end">%</InputAdornment>}
+        />
+        <FormHelperText>
+          <Tooltip
+            id="tooltip-icon"
+            title="The margin level the trader is required to have in order to take out a trade with the loan."
+          >
+            <ToolTipHint>Range: 10%-100%</ToolTipHint>
+          </Tooltip>
+        </FormHelperText>
+      </FormControl>
+      <FormControl>
+        <InputLabel>Liquidation Margin Amount *</InputLabel>
+        <Input
+          value={liquidationMarginAmount}
+          type="number"
+          onChange={setStateForInput(`liquidationMarginAmount`)}
+          endAdornment={<InputAdornment position="end">%</InputAdornment>}
+        />
+        <FormHelperText>
+          <Tooltip
+            id="tooltip-icon"
+            title="The margin level that will trigger a liquidation if the trader's margin balance falls to this level or lower. This cannot be greater than the initial margin amount."
+          >
+            <ToolTipHint>Range: 5%-95%</ToolTipHint>
+          </Tooltip>
+        </FormHelperText>
+      </FormControl>
     </div>
   </Section>
 );
-
-//   - Initial Margin Amount (initialMarginAmount) - the initial amount of margin the trader has to have to take out a trade with the loan
-//       - possible range 10%-100%
-//   - Liquidation Margin Amount (liquidationMarginAmount) - the margin level that will trigger a liquidation if the traders margin balance falls to this level or lower
-//       - possible range 5%-95% (note: this MUST be less than initialMarginAmount)
