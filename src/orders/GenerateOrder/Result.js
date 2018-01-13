91de2aba3b81039e385a68fd63f2f914b7421ec9
Adrian Li <li.adrianmc@gmail.com>
2018-01-13 14:07:45 -0500

    add styles to result component

--- src/orders/GenerateOrder/Result.js
+++ src/orders/GenerateOrder/Result.js
@@ -1,9 +1,42 @@
+/* eslint-disable react/no-unknown-property, no-return-assign */
 import { Fragment } from "react";
 import styled from "styled-components";
+import { COLORS } from "../../styles/constants";
+
+const HashDiv = styled.div`
+  margin-top: 24px;
+  text-align: center;
+  margin-bottom: 24px;
+`;
 
 const Hash = styled.div`
-  margin-top: 24px;
+  margin-top: 12px;
+  font-size: 12px;
+  color: ${COLORS.gray};
 `;
+
+const TextArea = styled.textarea`
+  width: 100%;
+  height: 480px;
+  font-family: monospace;
+`;
+
+class OrderObject extends React.Component {
+  handleClick = () => {
+    this.textarea.focus();
+    this.textarea.select();
+  };
+  render() {
+    return (
+      <TextArea
+        onClick={this.handleClick}
+        innerRef={x => (this.textarea = x)}
+        value={JSON.stringify(this.props.value, null, 4)}
+        readOnly
+      />
+    );
+  }
+}
 
 export default ({ orderHash, signedOrderObject }) => {
   if (!signedOrderObject) {
@@ -11,8 +44,10 @@ export default ({ orderHash, signedOrderObject }) => {
   }
   return (
     <Fragment>
-      <Hash>Order Hash: {orderHash}</Hash>
-      <pre>{JSON.stringify(signedOrderObject, null, 4)}</pre>
+      <HashDiv>
+        Order Hash: <Hash>{orderHash}</Hash>
+      </HashDiv>
+      <OrderObject value={signedOrderObject} />
     </Fragment>
   );
 };
