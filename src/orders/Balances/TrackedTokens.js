da953f9792f3c8f15bd20a234aeda6a33f9fcf4e
Adrian Li <li.adrianmc@gmail.com>
2018-01-24 14:04:58 -0500

    add functions for storing tracked tokens in local storage

--- src/orders/Balances/TrackedTokens.js
+++ src/orders/Balances/TrackedTokens.js
@@ -3,23 +3,32 @@ import styled from "styled-components";
 import Section, { SectionLabel } from "../../common/FormSection";
 import { getTokenInfoWithIcon } from "../../common/tokens";
 import TrackedTokenItem from "./TrackedTokenItem";
+import { getTrackedTokens } from "./utils";
 
 const Container = styled.div`
   width: 100%;
   max-width: 480px;
 `;
 
-const tokenAddresses = [`WETH_SM_ADDRESS_HERE`, `ZRX_SM_ADDRESS_HERE`];
+export default class TrackedTokens extends React.Component {
+  state = { trackedTokens: [] };
 
-const tempData = tokenAddresses
-  .map(getTokenInfoWithIcon)
-  .map(t => ({ ...t, amount: 10 }));
+  componentDidMount() {
+    const tokens = getTrackedTokens();
+    this.setState({ trackedTokens: tokens });
+  }
 
-export default () => (
-  <Section>
-    <SectionLabel>Tracked tokens</SectionLabel>
-    <Container>
-      {tempData.map(token => <TrackedTokenItem token={token} />)}
-    </Container>
-  </Section>
-);
+  render() {
+    const tokenData = this.state.trackedTokens
+      .map(getTokenInfoWithIcon)
+      .map(t => ({ ...t, amount: 10 }));
+    return (
+      <Section>
+        <SectionLabel>Tracked tokens</SectionLabel>
+        <Container>
+          {tokenData.map(token => <TrackedTokenItem token={token} />)}
+        </Container>
+      </Section>
+    );
+  }
+}
