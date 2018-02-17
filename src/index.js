c679afcf49593fa1388bbebba39b95783d1aa532
HL <36248745+honestbonsai@users.noreply.github.com>
2018-02-16 19:30:13 -0500

    Add sign order function

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import { assert } from "0x.js/lib/src/utils/assert";
+import { Web3Wrapper } from "@0xproject/web3-wrapper";
+import * as ethUtil from "ethereumjs-util";
 import { schemas, SchemaValidator } from "./schemas/b0x_json_schemas";
-
 import * as utils from "./utils";
 
 export default class B0xJS {
   static generatePseudoRandomSalt = utils.generatePseudoRandomSalt;
   static noop = utils.noop;
 
-  constructor(props) {
-    console.log(props);
+  constructor(provider) {
+    assert.isWeb3Provider("provider", provider);
+    this._web3Wrapper = new Web3Wrapper(provider);
   }
 
   // WARNING - this method is not supposed to be here,
@@ -33,5 +35,32 @@ export default class B0xJS {
     this.doesConformToSchema("loanOrder", order, schemas.loanOrderSchema);
     const orderHashHex = utils.getLoanOrderHashHex(order);
     return orderHashHex;
+  }
+
+  async signOrderHashAsync(
+    orderHash,
+    signerAddress,
+    shouldAddPersonalMessagePrefix
+  ) {
+    assert.isHexString("orderHash", orderHash);
+    await assert.isSenderAddressAsync(
+      "signerAddress",
+      signerAddress,
+      this._web3Wrapper
+    );
+
+    let msgHashHex = orderHash;
+    if (shouldAddPersonalMessagePrefix) {
+      const orderHashBuff = ethUtil.toBuffer(orderHash);
+      const msgHashBuff = ethUtil.hashPersonalMessage(orderHashBuff);
+      msgHashHex = ethUtil.bufferToHex(msgHashBuff);
+    }
+
+    const signature = await this._web3Wrapper.signTransactionAsync(
+      signerAddress,
+      msgHashHex
+    );
+
+    return signature;
   }
 }
