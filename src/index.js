7e645ec91081087e2a89b7e4f9aa7c05af80d240
HL <36248745+honestbonsai@users.noreply.github.com>
2018-02-19 12:26:28 -0500

    Use web3 directly instead of web3wrapper

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { assert } from "0x.js/lib/src/utils/assert";
-import { Web3Wrapper } from "@0xproject/web3-wrapper";
 import * as ethUtil from "ethereumjs-util";
+import Web3 from "web3";
 import { schemas, SchemaValidator } from "./schemas/b0x_json_schemas";
 import * as utils from "./utils";
 
@@ -10,7 +10,7 @@ export default class B0xJS {
 
   constructor(provider) {
     assert.isWeb3Provider("provider", provider);
-    this._web3Wrapper = new Web3Wrapper(provider);
+    this.web3 = new Web3(provider);
   }
 
   // WARNING - this method is not supposed to be here,
@@ -43,11 +43,6 @@ export default class B0xJS {
     shouldAddPersonalMessagePrefix
   ) {
     assert.isHexString("orderHash", orderHash);
-    await assert.isSenderAddressAsync(
-      "signerAddress",
-      signerAddress,
-      this._web3Wrapper
-    );
 
     let msgHashHex = orderHash;
     if (shouldAddPersonalMessagePrefix) {
@@ -56,10 +51,7 @@ export default class B0xJS {
       msgHashHex = ethUtil.bufferToHex(msgHashBuff);
     }
 
-    const signature = await this._web3Wrapper.signTransactionAsync(
-      signerAddress,
-      msgHashHex
-    );
+    const signature = await this.web3.eth.sign(msgHashHex, signerAddress);
 
     return signature;
   }
