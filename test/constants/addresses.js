ef76080c2917cf72388a2df22f72da26692d8fa1
HL <36248745+honestbonsai@users.noreply.github.com>
2018-02-24 12:38:26 -0500

    Add more test tokens and accounts

--- test/constants/addresses.js
+++ test/constants/addresses.js
@@ -1,3 +1,10 @@
-export const TEST_TOKENS = ["0xe1fefbd65ad6533aa202dc63005684538c494e94"];
-export const ACCOUNTS = ["0x5409ed021d9299bf6814279a6a1411a7e866a631"];
+export const TEST_TOKENS = [
+  "0xe1fefbd65ad6533aa202dc63005684538c494e94",
+  "0xa3fcf21aa86e9bb3688e8ffcb6a84d3b79aee479",
+  "0x5928a84ad4ba28ae1b7d979648c434864324ea1e"
+];
+export const ACCOUNTS = [
+  "0x5409ed021d9299bf6814279a6a1411a7e866a631",
+  "0x6ecbe1db9ef729cbe972c83fb886247691fb6beb"
+];
 export const B0x = "0x04758f1f88a9cea9bdef16d75f44c2f07a255e14";
