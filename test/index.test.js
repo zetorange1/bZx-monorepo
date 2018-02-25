a2306307d67243d65428fa603a4b6fcd3d918ba2
HL <36248745+honestbonsai@users.noreply.github.com>
2018-02-25 11:37:36 -0500

    Refactor tests

--- test/index.test.js
+++ test/index.test.js
@@ -18,61 +18,65 @@ const provider = new Web3.providers.HttpProvider(networkUrl);
 
 const b0xJS = new B0xJS(provider);
 
-test("signOrderHashAsync signs properly", async () => {
-  const order = {
-    b0xAddress: "0x0000000000000000000000000000000000000000",
-    makerAddress: "0x0000000000000000000000000000000000000000",
-    networkId: 1,
+describe("signOrderHashAsync", () => {
+  test("should sign properly", async () => {
+    const order = {
+      b0xAddress: "0x0000000000000000000000000000000000000000",
+      makerAddress: "0x0000000000000000000000000000000000000000",
+      networkId: 1,
 
-    // addresses
-    loanTokenAddress: "0x25b8fe1de9daf8ba351890744ff28cf7dfa8f5e3",
-    interestTokenAddress: "0x25b8fe1de9daf8ba351890744ff28cf7dfa8f5e3",
-    collateralTokenAddress: "0x25b8fe1de9daf8ba351890744ff28cf7dfa8f5e3",
-    feeRecipientAddress: "0x0000000000000000000000000000000000000000",
-    oracleAddress: "0x0000000000000000000000000000000000000000",
+      // addresses
+      loanTokenAddress: "0x25b8fe1de9daf8ba351890744ff28cf7dfa8f5e3",
+      interestTokenAddress: "0x25b8fe1de9daf8ba351890744ff28cf7dfa8f5e3",
+      collateralTokenAddress: "0x25b8fe1de9daf8ba351890744ff28cf7dfa8f5e3",
+      feeRecipientAddress: "0x0000000000000000000000000000000000000000",
+      oracleAddress: "0x0000000000000000000000000000000000000000",
 
-    // token amounts
-    loanTokenAmount: "40",
-    interestAmount: "41",
+      // token amounts
+      loanTokenAmount: "40",
+      interestAmount: "41",
 
-    // margin amounts
-    initialMarginAmount: "40",
-    maintenanceMarginAmount: "20",
+      // margin amounts
+      initialMarginAmount: "40",
+      maintenanceMarginAmount: "20",
 
-    // relay fees
-    lenderRelayFee: "0",
-    traderRelayFee: "0",
+      // relay fees
+      lenderRelayFee: "0",
+      traderRelayFee: "0",
 
-    // expiration date/time
-    expirationUnixTimestampSec: "1519061340",
-    salt: "0.2019429563929979"
-  };
+      // expiration date/time
+      expirationUnixTimestampSec: "1519061340",
+      salt: "0.2019429563929979"
+    };
 
-  const [signerAddress] = await b0xJS.web3.eth.getAccounts();
+    const [signerAddress] = await b0xJS.web3.eth.getAccounts();
 
-  const orderHash = B0xJS.getLoanOrderHashHex(order);
-  const signature = await b0xJS.signOrderHashAsync(orderHash, signerAddress);
+    const orderHash = B0xJS.getLoanOrderHashHex(order);
+    const signature = await b0xJS.signOrderHashAsync(orderHash, signerAddress);
 
-  // Not sure why this doesn't work
-  // const recoveredAccount = await b0xJS.web3.eth.accounts.recover(
-  //   orderHash,
-  //   signature
-  // );
-  const recoveredAccount = sigUtil.recoverPersonalSignature({
-    data: orderHash,
-    sig: signature
+    // Not sure why this doesn't work
+    // const recoveredAccount = await b0xJS.web3.eth.accounts.recover(
+    //   orderHash,
+    //   signature
+    // );
+    const recoveredAccount = sigUtil.recoverPersonalSignature({
+      data: orderHash,
+      sig: signature
+    });
+    expect(recoveredAccount).toBe(signerAddress.toLowerCase());
   });
-  expect(recoveredAccount).toBe(signerAddress.toLowerCase());
 });
 
-test("generatePseudoRandomSalt generates proper salt", () => {
-  const salt = B0xJS.generatePseudoRandomSalt();
-  expect(salt.gte(0)).toBe(true);
-  expect(salt.lt(1)).toBe(true);
+describe("generatePseudoRandomSalt", () => {
+  test("should generate proper salt", () => {
+    const salt = B0xJS.generatePseudoRandomSalt();
+    expect(salt.gte(0)).toBe(true);
+    expect(salt.lt(1)).toBe(true);
+  });
 });
 
 describe("getTokenContract", () => {
-  test("creates web3 contract instance", async () => {
+  test("should create web3 contract instance", async () => {
     const tokenContract = await utils.getTokenContract(
       b0xJS.web3,
       erc20Json,
@@ -84,7 +88,7 @@ describe("getTokenContract", () => {
     );
   });
 
-  test("throws error on incorrect address", async () => {
+  test("should throw error on incorrect address", async () => {
     await expect(
       utils.getTokenContract(b0xJS.web3, erc20Json, constants.ZERO_ADDRESS)
     ).rejects.toThrow(Errors.ContractDoesNotExist);
@@ -92,7 +96,7 @@ describe("getTokenContract", () => {
 });
 
 describe("getAllowance", () => {
-  test("returns allowance", async () => {
+  test("should return allowance", async () => {
     const res = await b0xJS.getAllowance({
       tokenAddress: Addresses.TEST_TOKENS[0],
       ownerAddress: Addresses.ACCOUNTS[0],
