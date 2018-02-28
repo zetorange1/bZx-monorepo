8b1c89f55b8e8a6f1a63f645bf4d959a1af02a9e
HL <36248745+honestbonsai@users.noreply.github.com>
2018-02-28 18:29:52 -0500

    Add assert

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,5 @@
 import { BigNumber } from "@0xproject/utils";
+import { assert } from "@0xproject/assert";
 import { constants } from "0x.js/lib/src/utils/constants";
 import BN from "bn.js";
 import Web3Utils from "web3-utils";
@@ -57,6 +58,7 @@ export const doesContractExistAtAddress = async (web3, address) => {
 };
 
 export const getContractInstance = async (web3, abi, address) => {
+  assert.isETHAddressHex("address", address);
   const contractExists = await doesContractExistAtAddress(web3, address);
   if (!contractExists) throw new Error(Errors.ContractDoesNotExist);
 
