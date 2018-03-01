6f2568ab9bde0c642e155fd1d44ab06f404c0750
HL <36248745+honestbonsai@users.noreply.github.com>
2018-02-28 20:57:36 -0500

    Make takeLoanOrderAsLender params less opaque

--- src/fill.js
+++ src/fill.js
@@ -4,15 +4,51 @@ import * as addresses from "../test/constants/addresses";
 
 export const takeLoanOrderAsLender = async (
   web3,
-  orderAddresses,
-  orderValues,
-  signature
+  {
+    // order addresses
+    makerAddress,
+    loanTokenAddress,
+    interestTokenAddress,
+    collateralTokenAddress,
+    feeRecipientAddress,
+    oracleAddress,
+    // order values
+    loanTokenAmount,
+    interestAmount,
+    initialMarginAmount,
+    maintenanceMarginAmount,
+    lenderRelayFee,
+    traderRelayFee,
+    expirationUnixTimestampSec,
+    salt,
+    signature
+  }
 ) => {
   const b0xContract = await utils.getContractInstance(
     web3,
     b0xAbi,
     addresses.B0x
   );
+
+  const orderAddresses = {
+    makerAddress,
+    loanTokenAddress,
+    interestTokenAddress,
+    collateralTokenAddress,
+    feeRecipientAddress,
+    oracleAddress
+  };
+
+  const orderValues = {
+    loanTokenAmount,
+    interestAmount,
+    initialMarginAmount,
+    maintenanceMarginAmount,
+    lenderRelayFee,
+    traderRelayFee,
+    expirationUnixTimestampSec,
+    salt
+  };
 
   b0xContract.methods
     .takeLoanOrderAsLender(orderAddresses, orderValues, signature)
--- test/fill.test.js
+++ test/fill.test.js
@@ -1,10 +1,11 @@
 /* globals test, describe */
 import b0xJS from "./setup";
+import order from "./constants/order";
 
 describe("filling orders", () => {
   describe("takeLoanOrderAsLender", async () => {
     test("should return total amount of loanToken borrowed", async () => {
-      await b0xJS.takeLoanOrderAsLender();
+      await b0xJS.takeLoanOrderAsLender(order);
     });
   });
 });
