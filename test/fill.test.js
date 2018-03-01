80985748a73f56ffb9feeaac0f696782e1983d1a
HL <36248745+honestbonsai@users.noreply.github.com>
2018-02-28 21:20:28 -0500

    Add required params to test

--- test/fill.test.js
+++ test/fill.test.js
@@ -1,11 +1,21 @@
 /* globals test, describe */
+import B0xJS from "../src";
 import b0xJS from "./setup";
+import * as addresses from "./constants/addresses";
 import order from "./constants/order";
 
 describe("filling orders", () => {
   describe("takeLoanOrderAsLender", async () => {
     test("should return total amount of loanToken borrowed", async () => {
-      await b0xJS.takeLoanOrderAsLender(order);
+      const signerAddress = addresses.ACCOUNTS[0];
+
+      const orderHashHex = B0xJS.getLoanOrderHashHex(order);
+      const signature = await b0xJS.signOrderHashAsync(
+        orderHashHex,
+        signerAddress
+      );
+      const res = await b0xJS.takeLoanOrderAsLender({ ...order, signature });
+      console.log(res);
     });
   });
 });
