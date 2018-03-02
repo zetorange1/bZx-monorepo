a61a5ac81f02bd8fac7d7512cfef67c0c600d8bb
Adrian Li <li.adrianmc@gmail.com>
2018-03-02 16:15:08 +0800

    replace oracle section with dropdown

--- src/orders/GenerateOrder/Oracle.js
+++ src/orders/GenerateOrder/Oracle.js
@@ -1,37 +1,43 @@
-import styled from "styled-components";
-import { FormControlLabel } from "material-ui/Form";
-import Checkbox from "material-ui/Checkbox";
-import TextField from "material-ui/TextField";
+// import styled from "styled-components";
+import { FormControl } from "material-ui/Form";
+// import TextField from "material-ui/TextField";
+import Select from "material-ui/Select";
+import { InputLabel } from "material-ui/Input";
+import { MenuItem } from "material-ui/Menu";
 import Section, { SectionLabel } from "../../common/FormSection";
 
-const AddressTextField = styled(TextField)`
-  max-width: 480px !important;
-`;
+// const AddressTextField = styled(TextField)`
+//   max-width: 480px !important;
+// `;
 
 export default ({
   oracleAddress,
   setOracleAddress,
-  useB0xCheckbox,
-  setUseB0xCheckbox
+  oracles = [
+    {
+      name: `b0xOracle`,
+      address: `0x0000000000000000000000000000000000000000`
+    },
+    {
+      name: `otherOracle`,
+      address: `0x1111111111111111111111111111111111111111`
+    }
+  ]
 }) => (
   <Section>
     <SectionLabel>Oracle</SectionLabel>
 
     <div>
-      <FormControlLabel
-        control={
-          <Checkbox checked={useB0xCheckbox} onChange={setUseB0xCheckbox} />
-        }
-        label="Use B0X Oracle"
-      />
+      <FormControl>
+        <InputLabel>Oracle</InputLabel>
+        <Select value={oracleAddress} onChange={setOracleAddress}>
+          {oracles.map(x => (
+            <MenuItem key={x.address} value={x.address}>
+              {x.name} ({x.address.slice(0, 10)}...)
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
     </div>
-    <AddressTextField
-      value={oracleAddress}
-      onChange={setOracleAddress}
-      label="Oracle Address"
-      margin="normal"
-      fullWidth
-      disabled={useB0xCheckbox}
-    />
   </Section>
 );
