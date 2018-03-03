b8873d9b084f049042caab429d8c7c8038f97689
HL <36248745+honestbonsai@users.noreply.github.com>
2018-03-03 12:21:37 -0500

    Refactor

--- test/fill.test.js
+++ test/fill.test.js
@@ -2,13 +2,13 @@
 import { pathOr } from "ramda";
 import B0xJS from "../src";
 import b0xJS from "./setup";
-import * as addresses from "./constants/addresses";
+import * as Addresses from "./constants/addresses";
 import makeOrder from "./utils/order";
 
 describe("filling orders", () => {
   describe("takeLoanOrderAsLender", async () => {
     test("should return total amount of loanToken borrowed", async () => {
-      const order = makeOrder();
+      const order = makeOrder({ makerAddress: Addresses.ACCOUNTS[0] });
       const signerAddress = order.makerAddress;
 
       const orderHashHex = B0xJS.getLoanOrderHashHex(order);
@@ -18,7 +18,7 @@ describe("filling orders", () => {
       );
       const receipt = await b0xJS.takeLoanOrderAsLender(
         { ...order, signature },
-        { from: addresses.ACCOUNTS[1], gas: 1000000 }
+        { from: Addresses.ACCOUNTS[1], gas: 1000000 }
       );
 
       const loanTokenAmountFilled = pathOr(
