01a336a85bc9ca3f51c6a95549983619df4cc19a
HL <36248745+honestbonsai@users.noreply.github.com>
2018-03-10 14:25:10 -0500

    Transfer and approve all relevant tokens and contracts

--- test/fill.test.js
+++ test/fill.test.js
@@ -7,9 +7,186 @@ import b0xJS from "./setup";
 import * as Addresses from "./constants/addresses";
 import makeOrder from "./utils/order";
 import * as orderConstants from "../src/constants/order";
+import * as utils from "../src/utils";
+import Contracts from "../src/contracts";
+
+const getContractInstances = contracts => {
+  const promises = contracts.map(contract =>
+    utils.getContractInstance(b0xJS.web3, contract.abi, contract.address)
+  );
+  return Promise.all(promises);
+};
+
+const initAllContractInstances = async () => {
+  const loanTokenContracts = [Contracts.TestToken0, Contracts.TestToken1];
+  const collateralTokenContracts = [Contracts.TestToken2, Contracts.TestToken3];
+  const interestTokenContracts = [Contracts.TestToken4, Contracts.TestToken5];
+
+  const loanTokens = await getContractInstances(loanTokenContracts);
+  const collateralTokens = await getContractInstances(collateralTokenContracts);
+  const interestTokens = await getContractInstances(interestTokenContracts);
+
+  const b0xToken = await utils.getContractInstance(
+    b0xJS.web3,
+    Contracts.B0xToken.abi,
+    Contracts.B0xToken.address
+  );
+  return { loanTokens, collateralTokens, interestTokens, b0xToken };
+};
+
+const setupB0xToken = ({
+  b0xToken,
+  lenders,
+  traders,
+  transferAmt,
+  ownerTxOpts
+}) => {
+  const promises = [
+    b0xToken.methods.transfer(lenders[0], transferAmt).send(ownerTxOpts),
+    b0xToken.methods.transfer(lenders[1], transferAmt).send(ownerTxOpts),
+    b0xToken.methods.transfer(traders[0], transferAmt).send(ownerTxOpts),
+    b0xToken.methods.transfer(traders[1], transferAmt).send(ownerTxOpts),
+    b0xJS.setAllowanceUnlimited({
+      tokenAddress: Contracts.B0xToken.address,
+      ownerAddress: lenders[0],
+      spenderAddress: Contracts.B0xVault.address
+    }),
+    b0xJS.setAllowanceUnlimited({
+      tokenAddress: Contracts.B0xToken.address,
+      ownerAddress: lenders[1],
+      spenderAddress: Contracts.B0xVault.address
+    }),
+    b0xJS.setAllowanceUnlimited({
+      tokenAddress: Contracts.B0xToken.address,
+      ownerAddress: traders[0],
+      spenderAddress: Contracts.B0xVault.address
+    }),
+    b0xJS.setAllowanceUnlimited({
+      tokenAddress: Contracts.B0xToken.address,
+      ownerAddress: traders[1],
+      spenderAddress: Contracts.B0xVault.address
+    })
+  ];
+  return Promise.all(promises);
+};
+
+const setupLoanTokens = ({ loanTokens, lenders, transferAmt, ownerTxOpts }) => {
+  const promises = [
+    loanTokens[0].methods.transfer(lenders[0], transferAmt).send(ownerTxOpts),
+    loanTokens[1].methods.transfer(lenders[1], transferAmt).send(ownerTxOpts),
+    b0xJS.setAllowanceUnlimited({
+      tokenAddress: loanTokens[0].options.address,
+      ownerAddress: lenders[0],
+      spenderAddress: Contracts.B0xVault.address
+    }),
+    b0xJS.setAllowanceUnlimited({
+      tokenAddress: loanTokens[1].options.address,
+      ownerAddress: lenders[1],
+      spenderAddress: Contracts.B0xVault.address
+    })
+  ];
+
+  return Promise.all(promises);
+};
+
+const setupCollateralTokens = ({
+  collateralTokens,
+  traders,
+  transferAmt,
+  ownerTxOpts
+}) => {
+  const promises = [
+    collateralTokens[0].methods
+      .transfer(traders[0], transferAmt)
+      .send(ownerTxOpts),
+    collateralTokens[1].methods
+      .transfer(traders[1], transferAmt)
+      .send(ownerTxOpts),
+    b0xJS.setAllowanceUnlimited({
+      tokenAddress: collateralTokens[0].options.address,
+      ownerAddress: traders[0],
+      spenderAddress: Contracts.B0xVault.address
+    }),
+    b0xJS.setAllowanceUnlimited({
+      tokenAddress: collateralTokens[1].options.address,
+      ownerAddress: traders[1],
+      spenderAddress: Contracts.B0xVault.address
+    })
+  ];
+
+  return Promise.all(promises);
+};
+
+const setupInterestTokens = ({
+  interestTokens,
+  traders,
+  transferAmt,
+  ownerTxOpts
+}) => {
+  const promises = [
+    interestTokens[0].methods
+      .transfer(traders[0], transferAmt)
+      .send(ownerTxOpts),
+    interestTokens[1].methods
+      .transfer(traders[1], transferAmt)
+      .send(ownerTxOpts),
+    b0xJS.setAllowanceUnlimited({
+      tokenAddress: interestTokens[0].options.address,
+      ownerAddress: traders[0],
+      spenderAddress: Contracts.B0xVault.address
+    }),
+    b0xJS.setAllowanceUnlimited({
+      tokenAddress: interestTokens[1].options.address,
+      ownerAddress: traders[1],
+      spenderAddress: Contracts.B0xVault.address
+    })
+  ];
+
+  return Promise.all(promises);
+};
 
 describe("filling orders", () => {
-  beforeAll(async () => {});
+  beforeAll(async () => {
+    const {
+      loanTokens,
+      collateralTokens,
+      interestTokens,
+      b0xToken
+    } = await initAllContractInstances();
+
+    const owner = Addresses.ACCOUNTS[0];
+    const lenders = [Addresses.ACCOUNTS[1], Addresses.ACCOUNTS[3]];
+    const traders = [Addresses.ACCOUNTS[2], Addresses.ACCOUNTS[4]];
+
+    const ownerTxOpts = { from: owner };
+    const transferAmt = b0xJS.web3.toWei(1000000, "ether");
+
+    await setupB0xToken({
+      b0xToken,
+      lenders,
+      traders,
+      transferAmt,
+      ownerTxOpts
+    });
+    await setupLoanTokens({
+      loanTokens,
+      lenders,
+      transferAmt,
+      ownerTxOpts
+    });
+    await setupCollateralTokens({
+      collateralTokens,
+      traders,
+      transferAmt,
+      ownerTxOpts
+    });
+    await setupInterestTokens({
+      interestTokens,
+      traders,
+      transferAmt,
+      ownerTxOpts
+    });
+  });
 
   describe("takeLoanOrderAsLender", async () => {
     test.skip("should return total amount of loanToken borrowed", async () => {
