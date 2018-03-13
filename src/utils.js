541af7f6b27522ad8794cd72aa501f3982703e16
HL <36248745+honestbonsai@users.noreply.github.com>
2018-03-12 21:38:22 -0400

    Make contract does not exist error msg clearer

--- src/utils.js
+++ src/utils.js
@@ -62,7 +62,8 @@ export const doesContractExistAtAddress = async (web3, address) => {
 export const getContractInstance = async (web3, abi, address) => {
   assert.isETHAddressHex("address", address);
   const contractExists = await doesContractExistAtAddress(web3, address);
-  if (!contractExists) throw new Error(Errors.ContractDoesNotExist);
+  if (!contractExists)
+    throw new Error(`${Errors.ContractDoesNotExist} ${address}`);
 
   const contract = new web3.eth.Contract(abi, address);
   return contract;
