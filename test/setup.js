cfa184fabe0761d574509e8f930c78a5d74f1fae
HL <36248745+honestbonsai@users.noreply.github.com>
2018-03-21 20:07:08 -0400

    Switch test network to local testnet

--- test/setup.js
+++ test/setup.js
@@ -2,7 +2,8 @@
 import Web3 from "web3";
 import B0xJS from "../src";
 
-const networkUrl = "https://testnet.b0x.network";
+// const networkUrl = "https://testnet.b0x.network";
+const networkUrl = "http://localhost:8545";
 const provider = new Web3.providers.HttpProvider(networkUrl);
 const b0xJS = new B0xJS(provider);
 
