5f118c172e13ad7419b890d526de5c94850b2151
HL <36248745+honestbonsai@users.noreply.github.com>
2018-03-31 11:58:16 -0400

    Add folder for ropsten contracts Refactor

--- src/contracts/index.js
+++ src/contracts/index.js
@@ -1,22 +1,5 @@
 import { map } from "ramda";
-import B0x from "./B0x.json";
-import B0xOracle from "./B0xOracle.json";
-import B0xTo0x from "./B0xTo0x.json";
-import B0xToken from "./B0xToken.json";
-import B0xVault from "./B0xVault.json";
-import EIP20 from "./EIP20.json";
-import OracleRegistry from "./OracleRegistry.json";
-import TestToken0 from "./TestToken0.json";
-import TestToken1 from "./TestToken1.json";
-import TestToken2 from "./TestToken2.json";
-import TestToken3 from "./TestToken3.json";
-import TestToken4 from "./TestToken4.json";
-import TestToken5 from "./TestToken5.json";
-import TestToken6 from "./TestToken6.json";
-import TestToken7 from "./TestToken7.json";
-import TestToken8 from "./TestToken8.json";
-import TestToken9 from "./TestToken9.json";
-import TokenRegistry from "./TokenRegistry.json";
+import ropsten from "./ropsten";
 
 import B0xTest from "../../../protocol_contracts/test_network/deployed/B0x.json";
 import B0xOracleTest from "../../../protocol_contracts/test_network/deployed/B0xOracle.json";
@@ -36,27 +19,6 @@ import TestToken7Test from "../../../protocol_contracts/test_network/deployed/Te
 import TestToken8Test from "../../../protocol_contracts/test_network/deployed/TestToken8.json";
 import TestToken9Test from "../../../protocol_contracts/test_network/deployed/TestToken9.json";
 import TokenRegistryTest from "../../../protocol_contracts/test_network/deployed/TokenRegistry.json";
-
-const live = {
-  B0x,
-  B0xOracle,
-  B0xTo0x,
-  B0xToken,
-  B0xVault,
-  EIP20,
-  OracleRegistry,
-  TestToken0,
-  TestToken1,
-  TestToken2,
-  TestToken3,
-  TestToken4,
-  TestToken5,
-  TestToken6,
-  TestToken7,
-  TestToken8,
-  TestToken9,
-  TokenRegistry
-};
 
 const test = {
   B0x: B0xTest,
@@ -79,7 +41,7 @@ const test = {
   TokenRegistry: TokenRegistryTest
 };
 
-const contractsRaw = process.env.NODE_ENV === "production" ? live : test;
+const contractsRaw = process.env.NODE_ENV === "production" ? ropsten : test;
 const contracts = map(
   ({ address, ...rest }) => ({ address: address.toLowerCase(), ...rest }),
   contractsRaw
