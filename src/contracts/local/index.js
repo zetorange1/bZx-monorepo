4bd8575bd2b9b3a6041f98116714447818165b68
HL <36248745+honestbonsai@users.noreply.github.com>
2018-03-31 13:16:54 -0400

    Fix contract names for local testnet being incorrect

--- src/contracts/local/index.js
+++ src/contracts/local/index.js
@@ -1,39 +1,39 @@
-import B0xTest from "../../../../protocol_contracts/test_network/deployed/B0x.json";
-import B0xOracleTest from "../../../../protocol_contracts/test_network/deployed/B0xOracle.json";
-import B0xTo0xTest from "../../../../protocol_contracts/test_network/deployed/B0xTo0x.json";
-import B0xTokenTest from "../../../../protocol_contracts/test_network/deployed/B0xToken.json";
-import B0xVaultTest from "../../../../protocol_contracts/test_network/deployed/B0xVault.json";
-import EIP20Test from "../../../../protocol_contracts/test_network/deployed/EIP20.json";
-import OracleRegistryTest from "../../../../protocol_contracts/test_network/deployed/OracleRegistry.json";
-import TestToken0Test from "../../../../protocol_contracts/test_network/deployed/TestToken0.json";
-import TestToken1Test from "../../../../protocol_contracts/test_network/deployed/TestToken1.json";
-import TestToken2Test from "../../../../protocol_contracts/test_network/deployed/TestToken2.json";
-import TestToken3Test from "../../../../protocol_contracts/test_network/deployed/TestToken3.json";
-import TestToken4Test from "../../../../protocol_contracts/test_network/deployed/TestToken4.json";
-import TestToken5Test from "../../../../protocol_contracts/test_network/deployed/TestToken5.json";
-import TestToken6Test from "../../../../protocol_contracts/test_network/deployed/TestToken6.json";
-import TestToken7Test from "../../../../protocol_contracts/test_network/deployed/TestToken7.json";
-import TestToken8Test from "../../../../protocol_contracts/test_network/deployed/TestToken8.json";
-import TestToken9Test from "../../../../protocol_contracts/test_network/deployed/TestToken9.json";
-import TokenRegistryTest from "../../../../protocol_contracts/test_network/deployed/TokenRegistry.json";
+import B0x from "../../../../protocol_contracts/test_network/deployed/B0x.json";
+import B0xOracle from "../../../../protocol_contracts/test_network/deployed/B0xOracle.json";
+import B0xTo0x from "../../../../protocol_contracts/test_network/deployed/B0xTo0x.json";
+import B0xToken from "../../../../protocol_contracts/test_network/deployed/B0xToken.json";
+import B0xVault from "../../../../protocol_contracts/test_network/deployed/B0xVault.json";
+import EIP20 from "../../../../protocol_contracts/test_network/deployed/EIP20.json";
+import OracleRegistry from "../../../../protocol_contracts/test_network/deployed/OracleRegistry.json";
+import TestToken0 from "../../../../protocol_contracts/test_network/deployed/TestToken0.json";
+import TestToken1 from "../../../../protocol_contracts/test_network/deployed/TestToken1.json";
+import TestToken2 from "../../../../protocol_contracts/test_network/deployed/TestToken2.json";
+import TestToken3 from "../../../../protocol_contracts/test_network/deployed/TestToken3.json";
+import TestToken4 from "../../../../protocol_contracts/test_network/deployed/TestToken4.json";
+import TestToken5 from "../../../../protocol_contracts/test_network/deployed/TestToken5.json";
+import TestToken6 from "../../../../protocol_contracts/test_network/deployed/TestToken6.json";
+import TestToken7 from "../../../../protocol_contracts/test_network/deployed/TestToken7.json";
+import TestToken8 from "../../../../protocol_contracts/test_network/deployed/TestToken8.json";
+import TestToken9 from "../../../../protocol_contracts/test_network/deployed/TestToken9.json";
+import TokenRegistry from "../../../../protocol_contracts/test_network/deployed/TokenRegistry.json";
 
 export default {
-  B0xTest,
-  B0xOracleTest,
-  B0xTo0xTest,
-  B0xTokenTest,
-  B0xVaultTest,
-  EIP20Test,
-  OracleRegistryTest,
-  TestToken0Test,
-  TestToken1Test,
-  TestToken2Test,
-  TestToken3Test,
-  TestToken4Test,
-  TestToken5Test,
-  TestToken6Test,
-  TestToken7Test,
-  TestToken8Test,
-  TestToken9Test,
-  TokenRegistryTest
+  B0x,
+  B0xOracle,
+  B0xTo0x,
+  B0xToken,
+  B0xVault,
+  EIP20,
+  OracleRegistry,
+  TestToken0,
+  TestToken1,
+  TestToken2,
+  TestToken3,
+  TestToken4,
+  TestToken5,
+  TestToken6,
+  TestToken7,
+  TestToken8,
+  TestToken9,
+  TokenRegistry
 };
