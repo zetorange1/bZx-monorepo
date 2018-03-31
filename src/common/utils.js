bf281f8e5c62c522b794d2ca6cd2c21f9b0afc09
Adrian Li <li.adrianmc@gmail.com>
2018-04-01 02:18:21 +0800

    wrap bignumber before assuming it is a bignumber

--- src/common/utils.js
+++ src/common/utils.js
@@ -2,10 +2,9 @@ import { BigNumber } from "bignumber.js";
 
 // const SCALE_FACTOR = BigNumber(1e18);
 
-export const fromBigNumber = (bigNum, scale = 1) =>
-  bigNum.div(scale).toNumber();
+export const fromBigNumber = (num, scale = 1) =>
+  BigNumber(num)
+    .div(scale)
+    .toNumber();
 
-export const toBigNumber = (num, scale = 1) => {
-  const bigNum = BigNumber(num);
-  return bigNum.times(scale);
-};
+export const toBigNumber = (num, scale = 1) => BigNumber(num).times(scale);
