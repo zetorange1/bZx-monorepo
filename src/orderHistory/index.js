a8254b60ceef8fcb2bfa48b10eb1bee6c8565000
HL <36248745+honestbonsai@users.noreply.github.com>
2018-04-01 15:11:16 -0400

    Use host in orderHistory fns

--- src/orderHistory/index.js
+++ src/orderHistory/index.js
@@ -4,11 +4,15 @@ import * as Addresses from "../addresses";
 import * as OrderUtils from "./utils/orders";
 import * as LoanPosUtils from "./utils/loanPositions";
 
-export const getOrders = async (web3, { loanPartyAddress, start, count }) => {
+export const getOrders = async (
+  web3,
+  host,
+  { loanPartyAddress, start, count }
+) => {
   const b0xContract = await CoreUtils.getContractInstance(
     web3,
-    getContracts(web3.currentProvider).B0x.abi,
-    Addresses.getAddresses(web3.currentProvider).B0x
+    getContracts(host).B0x.abi,
+    Addresses.getAddresses(host).B0x
   );
 
   const data = await b0xContract.methods
@@ -20,12 +24,13 @@ export const getOrders = async (web3, { loanPartyAddress, start, count }) => {
 
 export const getLoanPositions = async (
   web3,
+  host,
   { loanPartyAddress, start, count }
 ) => {
   const b0xContract = await CoreUtils.getContractInstance(
     web3,
-    getContracts(web3.currentProvider).B0x.abi,
-    Addresses.getAddresses(web3.currentProvider).B0x
+    getContracts(host).B0x.abi,
+    Addresses.getAddresses(host).B0x
   );
 
   const data = await b0xContract.methods
