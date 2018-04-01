21de29fbdc800a9d0feec5a7edb7d315143c442b
HL <36248745+honestbonsai@users.noreply.github.com>
2018-04-01 16:15:47 -0400

    Use networkId in test setup

--- src/core/__tests__/setup.js
+++ src/core/__tests__/setup.js
@@ -4,8 +4,9 @@ import B0xJS from "../index";
 
 // const networkUrl = "https://testnet.b0x.network";
 const networkUrl = "http://localhost:8545";
+const networkId = 50;
 const provider = new Web3.providers.HttpProvider(networkUrl);
-const b0xJS = new B0xJS(provider, { host: networkUrl });
+const b0xJS = new B0xJS(provider, { networkId });
 
 // Testnet mines about every 5-10 sec
 jest.setTimeout(100000);
