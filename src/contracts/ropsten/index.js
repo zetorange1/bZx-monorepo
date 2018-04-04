b4b129b62ea785a06fa164ec5ca2aa50a7aff5fe
William <zygroup1@gmail.com>
2018-04-04 13:09:04 -0400

    Added deployed Ropsten contracts.

--- src/contracts/ropsten/index.js
+++ src/contracts/ropsten/index.js
@@ -5,35 +5,24 @@ import B0xToken from "./B0xToken.json";
 import B0xVault from "./B0xVault.json";
 import EIP20 from "./EIP20.json";
 import OracleRegistry from "./OracleRegistry.json";
-import TestToken0 from "./TestToken0.json";
-import TestToken1 from "./TestToken1.json";
-import TestToken2 from "./TestToken2.json";
-import TestToken3 from "./TestToken3.json";
-import TestToken4 from "./TestToken4.json";
-import TestToken5 from "./TestToken5.json";
-import TestToken6 from "./TestToken6.json";
-import TestToken7 from "./TestToken7.json";
-import TestToken8 from "./TestToken8.json";
-import TestToken9 from "./TestToken9.json";
 import TokenRegistry from "./TokenRegistry.json";
 
+import ZRXToken from "./ZRXToken.json";
+import EtherToken from "./EtherToken.json";
+import TokenTransferProxy from "./TokenTransferProxy.json";
+import WETH9 from "./WETH9.json";
+
 export default {
-  B0x,
-  B0xOracle,
-  B0xTo0x,
-  B0xToken,
-  B0xVault,
-  EIP20,
-  OracleRegistry,
-  TestToken0,
-  TestToken1,
-  TestToken2,
-  TestToken3,
-  TestToken4,
-  TestToken5,
-  TestToken6,
-  TestToken7,
-  TestToken8,
-  TestToken9,
-  TokenRegistry
+	B0x,
+	B0xOracle,
+	B0xTo0x,
+	B0xToken,
+	B0xVault,
+	EIP20,
+	OracleRegistry,
+	TokenRegistry,
+	ZRXToken,
+	EtherToken,
+	TokenTransferProxy,
+	WETH9
 };
