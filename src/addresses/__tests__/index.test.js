c3c670deec04e7e6ed5c5f1966aaf7428e3049c5
HL <36248745+honestbonsai@users.noreply.github.com>
2018-04-08 17:19:04 -0400

    Add dummy tests for kovan and rinkeby

--- src/addresses/__tests__/index.test.js
+++ src/addresses/__tests__/index.test.js
@@ -3,6 +3,24 @@ import B0xJS from "../../core/index";
 import * as Errors from "../../core/constants/errors";
 
 describe("addresses", () => {
+  test("should return kovan testnet addresses for networkId 42", async () => {
+    const networkUrl = "https://kovan.infura.io";
+    const networkId = 42;
+    const provider = new Web3.providers.HttpProvider(networkUrl);
+    const b0xJS = new B0xJS(provider, { networkId });
+
+    expect(b0xJS.addresses).toMatchSnapshot();
+  });
+
+  test("should return rinkeby testnet addresses for networkId 4", async () => {
+    const networkUrl = "http://localhost:8545";
+    const networkId = 4;
+    const provider = new Web3.providers.HttpProvider(networkUrl);
+    const b0xJS = new B0xJS(provider, { networkId });
+
+    expect(b0xJS.addresses).toMatchSnapshot();
+  });
+
   test("should return local testnet addresses for all other networkIds", async () => {
     const networkUrl = "http://localhost:8545";
     const networkId = 50;
