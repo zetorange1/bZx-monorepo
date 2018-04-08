d0159fb8e6e4d3fa3843da2270f4d7720e545e7a
HL <36248745+honestbonsai@users.noreply.github.com>
2018-04-08 17:09:35 -0400

    Refactor local testnet test

--- src/addresses/__tests__/index.test.js
+++ src/addresses/__tests__/index.test.js
@@ -1,7 +1,14 @@
-import b0xJS from "../../core/__tests__/setup";
+/* globals jest */
+import Web3 from "web3";
+import B0xJS from "../../core/index";
 
 describe("addresses", () => {
-  test("should return an object of addresses", async () => {
+  test("should return local testnet addresses for all other networkIds", async () => {
+    const networkUrl = "http://localhost:8545";
+    const networkId = 50;
+    const provider = new Web3.providers.HttpProvider(networkUrl);
+    const b0xJS = new B0xJS(provider, { networkId });
+
     expect(b0xJS.addresses).toMatchSnapshot();
   });
 });
