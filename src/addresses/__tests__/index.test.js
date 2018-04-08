762724ca8a3d27d8b8fa38c81046a09a9acd91ce
HL <36248745+honestbonsai@users.noreply.github.com>
2018-04-08 17:13:51 -0400

    Add test for missing network id

--- src/addresses/__tests__/index.test.js
+++ src/addresses/__tests__/index.test.js
@@ -1,6 +1,6 @@
-/* globals jest */
 import Web3 from "web3";
 import B0xJS from "../../core/index";
+import * as Errors from "../../core/constants/errors";
 
 describe("addresses", () => {
   test("should return local testnet addresses for all other networkIds", async () => {
@@ -10,5 +10,14 @@ describe("addresses", () => {
     const b0xJS = new B0xJS(provider, { networkId });
 
     expect(b0xJS.addresses).toMatchSnapshot();
+  });
+
+  test("should return local testnet addresses for no networkId", async () => {
+    expect(() => {
+      const networkUrl = "http://localhost:8545";
+      const provider = new Web3.providers.HttpProvider(networkUrl);
+      // eslint-disable-next-line no-unused-vars
+      const b0xJS = new B0xJS(provider, { networkId: null });
+    }).toThrow(Errors.NoNetworkId);
   });
 });
--- src/core/constants/errors.js
+++ src/core/constants/errors.js
@@ -1,1 +1,2 @@
 export const ContractDoesNotExist = "Contract does not exist at address.";
+export const NoNetworkId = "Missing networkId. Provide a networkId param.";
