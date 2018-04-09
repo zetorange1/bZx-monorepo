b3e451ef4771665e1d587a74721f2c617a7f8a90
HL <36248745+honestbonsai@users.noreply.github.com>
2018-04-09 18:57:58 -0400

    Refactor tokenRegistry

--- src/tokenRegistry/index.js
+++ src/tokenRegistry/index.js
@@ -1,32 +1,37 @@
-import { assert } from "@0xproject/assert";
 import * as utils from "../core/utils";
 import { getContracts } from "../contracts";
 import * as Addresses from "../addresses";
 
 export const getTokenList = async ({ web3, networkId }) => {
-
   const tokenRegistryContract = await utils.getContractInstance(
     web3,
     getContracts(networkId).TokenRegistry.abi,
     Addresses.getAddresses(networkId).TokenRegistry
   );
 
-  const tokenAddresses = await tokenRegistryContract.methods.getTokenAddresses().call();
+  const tokenAddresses = await tokenRegistryContract.methods
+    .getTokenAddresses()
+    .call();
 
-  var tokens = [];
-  for(var i=0; i < tokenAddresses.length; i++) {
-    const contractExists = await utils.doesContractExistAtAddress(web3, tokenAddresses[i]);
-    if (contractExists) {
-      const tokenData = await tokenRegistryContract.methods.getTokenMetaData(tokenAddresses[i]).call();
-      tokens.push({
-        "address": tokenData[0].toLowerCase(),
-        "name": tokenData[1],
-        "symbol": tokenData[2],
-        "decimals": tokenData[3],
-        "url": tokenData[4],
-      });
+  const promises = tokenAddresses.map(async address => {
+    const doesExist = await utils.doesContractExistAtAddress(web3, address);
+    if (doesExist) {
+      const tokenData = await tokenRegistryContract.methods
+        .getTokenMetaData(address)
+        .call();
+      return {
+        address: tokenData[0].toLowerCase(),
+        name: tokenData[1],
+        symbol: tokenData[2],
+        decimals: tokenData[3],
+        url: tokenData[4]
+      };
     }
-  }
+    return null;
+  });
+
+  const tokensRaw = await Promise.all(promises);
+  const tokens = tokensRaw.filter(token => !!token);
 
   return tokens;
-}
+};
