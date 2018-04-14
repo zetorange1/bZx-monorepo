30294ef26ecd09d3891a347d3d76c2b12717e432
HL <36248745+honestbonsai@users.noreply.github.com>
2018-04-13 23:16:49 -0400

    Refactor

--- src/orderHistory/utils/loanPositions.js
+++ src/orderHistory/utils/loanPositions.js
@@ -2,20 +2,15 @@ import { pipe, map } from "ramda";
 import * as Utils from "./index";
 
 const NUM_LOAN_POS_FIELDS = 9;
-const HEX_RADIX = 16;
-
-const prepend0x = arg => `0x${arg}`;
-const substr24 = arg => arg.substr(24);
-const parseInt16 = arg => parseInt(arg, HEX_RADIX);
 
 const getLoanPosition = params => ({
-  ...map(pipe(substr24, prepend0x), {
+  ...map(pipe(Utils.substr24, Utils.prepend0x), {
     lender: params[0],
     trader: params[1],
     collateralTokenAddressFilled: params[2],
     positionTokenAddressFilled: params[3]
   }),
-  ...map(pipe(prepend0x, parseInt16), {
+  ...map(pipe(Utils.prepend0x, Utils.parseIntHex), {
     loanTokenAmountFilled: params[4],
     collateralTokenAmountFilled: params[5],
     positionTokenAmountFilled: params[6],
