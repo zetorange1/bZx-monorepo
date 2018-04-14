714738af0ecae9c256a8ad055d83ff2546523b40
HL <36248745+honestbonsai@users.noreply.github.com>
2018-04-13 23:10:21 -0400

    Refactor

--- src/orderHistory/utils/loanPositions.js
+++ src/orderHistory/utils/loanPositions.js
@@ -4,27 +4,35 @@ import * as Utils from "./index";
 const NUM_LOAN_POS_FIELDS = 9;
 const HEX_RADIX = 16;
 
+const prepend0x = arg => `0x${arg}`;
+const substr24 = arg => arg.substr(24);
+const parseInt16 = arg => parseInt(arg, HEX_RADIX);
+
 const getLoanPosition = params => ({
-  lender: `0x${params[0].substr(24)}`,
-  trader: `0x${params[1].substr(24)}`,
-  collateralTokenAddressFilled: `0x${params[2].substr(24)}`,
-  positionTokenAddressFilled: `0x${params[3].substr(24)}`,
-  loanTokenAmountFilled: parseInt(`0x${params[4]}`, HEX_RADIX),
-  collateralTokenAmountFilled: parseInt(`0x${params[5]}`, HEX_RADIX),
-  positionTokenAmountFilled: parseInt(`0x${params[6]}`, HEX_RADIX),
-  loanStartUnixTimestampSec: parseInt(`0x${params[7]}`, HEX_RADIX),
-  active: parseInt(`0x${params[8]}`, HEX_RADIX)
+  ...map(pipe(substr24, prepend0x), {
+    lender: params[0],
+    trader: params[1],
+    collateralTokenAddressFilled: params[2],
+    positionTokenAddressFilled: params[3]
+  }),
+  ...map(pipe(prepend0x, parseInt16), {
+    loanTokenAmountFilled: params[4],
+    collateralTokenAmountFilled: params[5],
+    positionTokenAmountFilled: params[6],
+    loanStartUnixTimestampSec: params[7],
+    active: params[8]
+  })
 });
 
 const checkProperObjCount = Utils.makeCheckProperObjCount(NUM_LOAN_POS_FIELDS);
 const getOrderObjArray = Utils.makeGetOrderObjArray(NUM_LOAN_POS_FIELDS);
 
 export const cleanData = raw =>
-  raw ?
-  pipe(
-    Utils.remove0xPrefix,
-    checkProperObjCount,
-    getOrderObjArray,
-    map(pipe(Utils.getOrderParams, getLoanPosition))
-  )(raw) :
-  [];
+  raw
+    ? pipe(
+        Utils.remove0xPrefix,
+        checkProperObjCount,
+        getOrderObjArray,
+        map(pipe(Utils.getOrderParams, getLoanPosition))
+      )(raw)
+    : [];
