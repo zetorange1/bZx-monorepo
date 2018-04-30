3fcb265bd2b69ccc48cbfae74e32b66b8fcb818d
HL <36248745+honestbonsai@users.noreply.github.com>
2018-04-29 21:53:46 -0400

    Add depositCollateral fn

--- src/core/index.js
+++ src/core/index.js
@@ -112,4 +112,6 @@ export default class B0xJS {
   tradePositionWith0x = (...props) => trade.tradePositionWith0x(this, ...props);
 
   changeCollateral = (...props) => loanHealth.changeCollateral(this, ...props);
+  depositCollateral = (...props) =>
+    loanHealth.depositCollateral(this, ...props);
 }
