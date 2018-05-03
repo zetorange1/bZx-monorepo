67f4d7d6f7e660dad8dd816e1874497c08a1d908
Adrian Li <li.adrianmc@gmail.com>
2018-05-03 12:48:23 +0900

    fix linitng

--- src/common/Nav.js
+++ src/common/Nav.js
@@ -57,7 +57,7 @@ const Version = styled.div`
 
 const Nav = () => (
   <div>
-    <div className="network-indicator"></div>
+    <div className="network-indicator" />
     <Version>Alpha v{packageJson.version}</Version>
     <Container>
       <Content>
--- src/common/NetworkIndicator.js
+++ src/common/NetworkIndicator.js
@@ -1,3 +1,4 @@
+/* globals document */
 import ReactDOM from "react-dom";
 import styled from "styled-components";
 
@@ -11,24 +12,32 @@ const NetworkName = styled.span`
 `;
 
 const networks = {
-  1: { name: "Main Ethereum Network", color: "#038789" },
-  3: { name: "Ropsten Test Network", color: "#E91550" },
-  4: { name: "Rinkeby Test Network", color: "#EBB33F"},
-  42: { name: "Kovan Test Network", color: "#690496"}
+  1: { name: `Main Ethereum Network`, color: `#038789` },
+  3: { name: `Ropsten Test Network`, color: `#E91550` },
+  4: { name: `Rinkeby Test Network`, color: `#EBB33F` },
+  42: { name: `Kovan Test Network`, color: `#690496` }
 };
 
 const NetworkIndicator = ({ networkId }) => {
-  const nameExists = networks.hasOwnProperty(networkId)
-  const domNode = document.getElementsByClassName("network-indicator")[0];
+  // eslint-disable-next-line no-prototype-builtins
+  const nameExists = networks.hasOwnProperty(networkId);
+  const domNode = document.getElementsByClassName(`network-indicator`)[0];
 
   let ToRender;
   if (nameExists) {
-    ToRender = <Container>Network: <NetworkName color={networks[networkId].color}>{networks[networkId].name}</NetworkName></Container>
+    ToRender = (
+      <Container>
+        Network:{` `}
+        <NetworkName color={networks[networkId].color}>
+          {networks[networkId].name}
+        </NetworkName>
+      </Container>
+    );
   } else {
-    ToRender = <Container>Custom Network: {networkId}</Container>
+    ToRender = <Container>Custom Network: {networkId}</Container>;
   }
 
   return ReactDOM.createPortal(ToRender, domNode);
 };
 
-export default NetworkIndicator
+export default NetworkIndicator;
