911df692bd4ed4dc5e06944d69ca014ab2f063cf
Adrian Li <li.adrianmc@gmail.com>
2018-05-06 18:30:57 +0900

    resolve conflicts

--- src/borrowing/CollateralOptions.js
+++ src/borrowing/CollateralOptions.js
@@ -41,7 +41,7 @@ export default class CollateralOptions extends React.Component {
 
   render() {
     const { anchorEl } = this.state;
-    const { b0x, tokens, accounts, web3, loanOrderHash } = this.props;
+    const { b0x, tokens, accounts, web3, loanOrderHash, collateralToken } = this.props;
     return (
       <Fragment>
         <Button variant="raised" onClick={this.handleClick}>
@@ -65,18 +65,26 @@ export default class CollateralOptions extends React.Component {
         <ChangeCollateralDialog
           open={this.state.showChangeCollateralDialog}
           onClose={this.closeDialog(`showChangeCollateralDialog`)}
+<<<<<<< Updated upstream
           b0x={b0x}
           web3={web3}
           tokens={tokens}
           accounts={accounts}
           loanOrderHash={loanOrderHash}
+=======
+          tokens={tokens}
+>>>>>>> Stashed changes
         />
         <DepositCollateralDialog
           open={this.state.showDepositCollateralDialog}
           onClose={this.closeDialog(`showDepositCollateralDialog`)}
+<<<<<<< Updated upstream
           b0x={b0x}
           tokens={tokens}
           accounts={accounts}
+=======
+          collateralToken={collateralToken}
+>>>>>>> Stashed changes
         />
         <WithdrawCollateralDialog
           open={this.state.showWithdrawCollateralDialog}
