0a3077e7fb895cb23ce3a82761ee6d3a183be3c5
Adrian Li <li.adrianmc@gmail.com>
2018-05-10 03:35:36 +0900

    add pagination for loans on borrowing page

--- src/borrowing/index.js
+++ src/borrowing/index.js
@@ -1,33 +1,66 @@
-/* eslint-disable */
+import styled from "styled-components";
+import MuiButton from "material-ui/Button";
+
 import Section, { SectionLabel } from "../common/FormSection";
 import OpenLoan from "./OpenLoan";
-import ClosedLoan from "./ClosedLoan";
+// import ClosedLoan from "./ClosedLoan";
 
-const openLoans = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
+const InfoContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-const closedLoans = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }];
+const ShowCount = styled.div`
+  display: inline-block;
+  margin: 6px;
+`;
+
+const Button = styled(MuiButton)`
+  margin: 6px !important;
+`;
 
 export default class Borrowing extends React.Component {
-  state = { positions: [] };
+  state = { loans: [], loading: false, count: 10 };
 
-  async componentDidMount() {
+  componentDidMount() {
+    this.getLoans();
+  }
+
+  getLoans = async () => {
     const { b0x, accounts } = this.props;
-    const positions = await b0x.getLoansForTrader({
+    this.setState({ loans: [], loading: true });
+    const loans = await b0x.getLoansForTrader({
       address: accounts[0],
       start: 0,
-      count: 10
+      count: this.state.count
     });
-    console.log(positions)
-    this.setState({ positions });
-  }
+    console.log(loans);
+    this.setState({ loans, loading: false });
+  };
+
+  increaseCount = () => {
+    this.setState(p => ({ count: p.count + 10 }), this.getLoans);
+  };
 
   render() {
     const { b0x, tokens, accounts, web3 } = this.props;
-    const { positions } = this.state;
-    const openPositions = positions.filter(p => p.active === 1);
-    const closedPositions = positions.filter(p => p.active === 0);
+    const { loans, loading, count } = this.state;
+    const openPositions = loans.filter(p => p.active === 1);
+    // const closedPositions = loans.filter(p => p.active === 0);
     return (
       <div>
+        <InfoContainer>
+          <ShowCount>
+            Showing last {count} loans ({loans.length} loans found).
+          </ShowCount>
+          <Button onClick={this.increaseCount} variant="raised" color="primary">
+            Show more
+          </Button>
+          <Button onClick={this.getOrders} variant="raised" disabled={loading}>
+            {loading ? `Refreshing...` : `Refresh`}
+          </Button>
+        </InfoContainer>
+        <br />
         <Section>
           <SectionLabel>Open Loans</SectionLabel>
           {openPositions.map(data => (
