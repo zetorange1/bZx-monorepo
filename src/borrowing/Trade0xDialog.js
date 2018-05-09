6eae18ebbd986f2e5061fe3e40157320edd782c1
Adrian Li <li.adrianmc@gmail.com>
2018-05-10 03:25:22 +0900

    setup dialog for 0x orders

--- src/borrowing/Trade0xDialog.js
+++ src/borrowing/Trade0xDialog.js
@@ -16,24 +16,48 @@ import Section, { SectionLabel, Divider } from "../common/FormSection";
 // }
 // `;
 
+const TextArea = styled.textarea`
+  margin: 12px 0;
+  width: 480px;
+  max-width: 100%;
+  font-family: monospace;
+`;
+
 export default class Trade0xDialog extends React.Component {
+  state = { value: `` };
+
+  handleChange = e => this.setState({ value: e.target.value });
+
+  executeTrade = async () => {
+    const { value } = this.state;
+    const order0x = JSON.parse(value);
+    console.log(order0x);
+  };
+
   render() {
+    const { value } = this.state;
     return (
       <Dialog open={this.props.open} onClose={this.props.onClose}>
         <DialogTitle>Execute Trade with 0x Order</DialogTitle>
         <DialogContent>
           <Section>
             <SectionLabel>1. Paste in your 0x order</SectionLabel>
+            <TextArea
+              cols="30"
+              rows="10"
+              value={value}
+              onChange={this.handleChange}
+            />
           </Section>
           <Divider />
           <Section>
-            <SectionLabel>2. Approve the token</SectionLabel>
-          </Section>
-          <Divider />
-          <Section>
-            <SectionLabel>3. Execute the change</SectionLabel>
-            <p>Explanation here.</p>
-            <Button variant="raised" color="primary">
+            <SectionLabel>2. Execute the trade</SectionLabel>
+            <p>
+              This function is for the user to present a pre-existing 0x order
+              found elsewhere to b0x, so b0x can be the taker on behalf of the
+              user.
+            </p>
+            <Button onClick={this.executeTrade} variant="raised" color="primary">
               Execute trade
             </Button>
           </Section>
