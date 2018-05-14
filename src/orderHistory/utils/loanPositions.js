fd66eb693c87f0711b2884a00350e4c84103b7ab
HL <36248745+honestbonsai@users.noreply.github.com>
2018-05-14 16:56:44 -0400

    Refactor

--- src/orderHistory/utils/loanPositions.js
+++ src/orderHistory/utils/loanPositions.js
@@ -2,24 +2,29 @@ import { pipe, map } from "ramda";
 import * as Utils from "./index";
 
 const NUM_LOAN_POS_FIELDS = 15;
-const HEX_RADIX = 16;
 
 const getLoanPosition = params => ({
-  lender: `0x${params[0].substr(24)}`,
-  trader: `0x${params[1].substr(24)}`,
-  collateralTokenAddressFilled: `0x${params[2].substr(24)}`,
-  positionTokenAddressFilled: `0x${params[3].substr(24)}`,
-  loanTokenAmountFilled: parseInt(`0x${params[4]}`, HEX_RADIX),
-  collateralTokenAmountFilled: parseInt(`0x${params[5]}`, HEX_RADIX),
-  positionTokenAmountFilled: parseInt(`0x${params[6]}`, HEX_RADIX),
-  loanStartUnixTimestampSec: parseInt(`0x${params[7]}`, HEX_RADIX),
-  index: parseInt(`0x${params[8]}`, HEX_RADIX),
-  active: parseInt(`0x${params[9]}`, HEX_RADIX),
-  loanOrderHash: `0x${params[10]}`,
-  loanTokenAddress: `0x${params[11].substr(24)}`,
-  interestTokenAddress: `0x${params[12].substr(24)}`,
-  interestTotalAccrued: parseInt(`0x${params[13]}`, HEX_RADIX),
-  interestPaidSoFar: parseInt(`0x${params[14]}`, HEX_RADIX)
+  ...map(pipe(Utils.substr24, Utils.prepend0x), {
+    lender: params[0],
+    trader: params[1],
+    collateralTokenAddressFilled: params[2],
+    positionTokenAddressFilled: params[3],
+    loanTokenAddress: params[11],
+    interestTokenAddress: params[12]
+  }),
+  ...map(pipe(Utils.prepend0x, Utils.parseIntHex), {
+    loanTokenAmountFilled: params[4],
+    collateralTokenAmountFilled: params[5],
+    positionTokenAmountFilled: params[6],
+    loanStartUnixTimestampSec: params[7],
+    index: params[8],
+    active: params[9],
+    interestTotalAccrued: params[13],
+    interestPaidSoFar: params[14]
+  }),
+  ...map(pipe(Utils.prepend0x), {
+    loanOrderHash: params[10]
+  })
 });
 
 const checkProperObjCount = Utils.makeCheckProperObjCount(NUM_LOAN_POS_FIELDS);
