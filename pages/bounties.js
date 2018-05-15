9db541b975a3c138d49ae0b476e15aeaade6bb5a
Adrian Li <li.adrianmc@gmail.com>
2018-05-15 11:05:34 +0900

    fetch loans

--- pages/bounties.js
+++ pages/bounties.js
@@ -1,7 +1,10 @@
+import { Fragment } from "react";
 import Layout from "../src/common/Layout";
 import { Card, Header, HeaderTitle, Content } from "../src/common/MainContent";
 import { Divider } from "../src/common/FormSection";
 import PageContent from "../src/bounties";
+import Web3Container from "../src/web3/Web3Container";
+import NetworkIndicator from "../src/common/NetworkIndicator";
 
 export default class Bounties extends React.Component {
   state = { activeTab: undefined };
@@ -17,15 +20,36 @@ export default class Bounties extends React.Component {
             <HeaderTitle>Bounties</HeaderTitle>
           </Header>
           <Content>
-            This section will allow bounty hunters to liquidate loans that have
-            become unsafe. Unsafe loans are open loans where the value of the
-            collateral has fallen below the requisite amount (i.e. the
-            maintenance margin amount).
-            <Divider />
-            <PageContent />
+            <Web3Container
+              render={({ web3, zeroEx, tokens, b0x, accounts, networkId }) => (
+                <Fragment>
+                  <NetworkIndicator
+                    networkId={networkId}
+                    accounts={accounts}
+                    etherscanURL={b0x.etherscanURL}
+                  />
+                  <p>
+                    This section will allow bounty hunters to liquidate loans
+                    that have become unsafe. Unsafe loans are open loans where
+                    the value of the collateral has fallen below the requisite
+                    amount (i.e. the maintenance margin amount).
+                  </p>
+                  <Divider />
+                  <PageContent
+                    web3={web3}
+                    zeroEx={zeroEx}
+                    b0x={b0x}
+                    accounts={accounts}
+                    tokens={tokens}
+                  />
+                </Fragment>
+              )}
+            />
           </Content>
         </Card>
       </Layout>
     );
   }
 }
+
+// b0x, tokens, accounts, web3
