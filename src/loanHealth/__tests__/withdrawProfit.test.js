cd0bb30b4d4acf2143efd14919fbbd445068708c
HL <36248745+honestbonsai@users.noreply.github.com>
2018-06-07 23:23:19 -0400

    Add passing test for withdrawing profit successfully

--- src/loanHealth/__tests__/withdrawProfit.test.js
+++ src/loanHealth/__tests__/withdrawProfit.test.js
@@ -1,64 +1,23 @@
 import { pathOr } from "ramda";
-import B0xJS from "../../core";
 import b0xJS from "../../core/__tests__/setup";
 import { expectPromiEvent } from "../../core/__tests__/utils";
-import * as FillTestUtils from "../../fill/__tests__/utils";
 
 const { web3 } = b0xJS;
 
 describe("loanHeath", () => {
   describe("withdrawProfit", () => {
-    const { owner, lenders, traders } = FillTestUtils.getAccounts();
-    const {
-      loanTokens,
-      interestTokens,
-      collateralTokens
-    } = FillTestUtils.initAllContractInstances();
-
     let promiEvent = null;
-    let loanOrderHash = null;
-
-    const collateralTokenFilled = collateralTokens[0].options.address.toLowerCase();
-
-    let order = null;
+    // Seed data values with profit
+    const loanOrderHash =
+      "0x675aa699238281d7bc01b954e6f4ccc960162c612b77145efb1498c76728ce3b";
+    const trader = "0xe834ec434daba538cd1b9fe1582052b880bd7e63";
 
     beforeAll(async () => {
-      await FillTestUtils.setupAll({
-        owner,
-        lenders,
-        traders,
-        transferAmount: web3.utils.toWei("100", "ether")
-      });
-
-      order = FillTestUtils.makeOrderAsLender({
-        web3,
-        lenders,
-        loanTokens,
-        interestTokens
-      });
-      loanOrderHash = B0xJS.getLoanOrderHashHex(order);
-
-      const signature = await b0xJS.signOrderHashAsync(
-        loanOrderHash,
-        order.makerAddress
-      );
       const txOpts = {
-        from: traders[0],
+        from: trader,
         gas: 1000000,
         gasPrice: web3.utils.toWei("5", "gwei").toString()
       };
-      const loanTokenAmountFilled = web3.utils.toWei("12.3");
-      // b0x hash that we give to tradePositionWith0x must belong to a loan that was previously filled, so we fill the loan order here
-      const takeLoanOrderAsTraderReceipt = await b0xJS.takeLoanOrderAsTrader(
-        { ...order, signature },
-        collateralTokenFilled,
-        loanTokenAmountFilled,
-        txOpts
-      );
-
-      expect(
-        pathOr(null, ["events", "DebugLine"], takeLoanOrderAsTraderReceipt)
-      ).toEqual(null);
 
       promiEvent = b0xJS.withdrawProfit({
         loanOrderHash,
@@ -68,6 +27,12 @@ describe("loanHeath", () => {
 
     test("should return promiEvent", () => {
       expectPromiEvent(promiEvent);
+    });
+
+    test("should withdraw profit successfully", async () => {
+      const receipt = await promiEvent;
+      const event = pathOr(null, ["events", "LogWithdrawProfit"], receipt);
+      expect(event).not.toEqual(null);
     });
   });
 });
