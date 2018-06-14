81bcf069e2c83112754da20fa8aaa3bda5208670
William <zygroup1@gmail.com>
2018-06-14 14:23:51 -0400

    Added WETH9 ABI to contract assets.

--- extra/generate_test_assets.js
+++ extra/generate_test_assets.js
@@ -49,6 +49,10 @@ var addresses_0x = {
 	"ZRXToken": web3utils.toChecksumAddress(config["addresses"][network]["ZeroEx"]["ZRXToken"]),
 	"WETH": config["addresses"][network]["ZeroEx"]["WETH9"] != "" ? web3utils.toChecksumAddress(config["addresses"][network]["ZeroEx"]["WETH9"]) : web3utils.toChecksumAddress(config["addresses"][network]["ZeroEx"]["EtherToken"]),
 }
+var abis_0x = {
+	"ZRXToken": '[]',
+	"WETH": '[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Withdrawal","type":"event"}]',
+}
 
 if (network != "mainnet") {
 	if (network != "ropsten") {
@@ -231,7 +235,11 @@ outHTML += `
 
 `;
 Object.keys(addresses_0x).forEach(function(item, index) {
-	outHTML += item + " :: " + addresses_0x[item] + "\n";
+	if (abis_0x[item] != "[]") {
+		outHTML += item + " :: " + addresses_0x[item] + " <a href=\"abi/"+item+".abi.json\" target=\"_blank\">abi</a>\n";
+	} else {
+		outHTML += item + " :: " + addresses_0x[item] + "\n";
+	}
 });
 outHTML += `
 
@@ -246,11 +254,18 @@ fs.writeFile("./html_public_test/index.html", outHTML, function(err) {
 	}
 });
 
-Object.keys(addresses_0x).forEach(function(item, index) {
+Object.keys(abis_0x).forEach(function(item, index) {
+	if (abis_0x[item] != "[]") {
+		fs.writeFile("./html_public_test/abi/"+item+".abi.json", JSON.stringify(JSON.parse(abis_0x[item]), null, '\t'), function(err) {
+			if(err) {
+				console.log(item+".json Error: "+err);
+			}
+		});
+	}
 	fs.writeFileSync("./html_public_test/deployed/"+item+".json", JSON.stringify({
 		"name": item,
 		"address": addresses_0x[item],
-		"abi": []
+		"abi": JSON.parse(abis_0x[item])
 	}), function(err) {
 		if(err) {
 			console.log(item+".json Error: "+err);
