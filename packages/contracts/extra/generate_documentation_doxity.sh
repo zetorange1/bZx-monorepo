e3cb39473667fc7afd1f0dacf38ea32c8e116245
Alex Shafranovich <alexey.shafranovich@live.com>
2018-09-26 16:17:12 +0300

    remove truffle.js as it breaks builds on windows systems

--- packages/contracts/extra/generate_documentation_doxity.sh
+++ packages/contracts/extra/generate_documentation_doxity.sh
@@ -2,13 +2,21 @@
 
 CURDIR=`/bin/pwd`
 OPENZEPPELIN="node_modules/openzeppelin-solidity"
+TRUFFLE_CONFIG_JS="truffle-config.js"
+TRUFFLE_JS="truffle.js"
 
 if [[ -L "$CURDIR/$OPENZEPPELIN" && -d "$CURDIR/$OPENZEPPELIN" ]]
 then
     unlink $CURDIR/$OPENZEPPELIN
 fi
 
+if [[ -L "$CURDIR/$TRUFFLE_JS" && -f "$CURDIR/$TRUFFLE_JS" ]]
+then
+    unlink $CURDIR/$TRUFFLE_JS
+fi
+
 ln -s $CURDIR/../../$OPENZEPPELIN $CURDIR/$OPENZEPPELIN
+ln -s $CURDIR/$TRUFFLE_CONFIG_JS $CURDIR/$TRUFFLE_JS
 
 if [ ! -f $CURDIR/scripts/doxity/pages/docs ]; then
     mkdir -p $CURDIR/scripts/doxity/pages/docs
@@ -19,3 +27,4 @@ command="$CURDIR/node_modules/.bin/doxity build"
 eval "$command"
 
 unlink $CURDIR/$OPENZEPPELIN
+unlink $CURDIR/$TRUFFLE_JS
