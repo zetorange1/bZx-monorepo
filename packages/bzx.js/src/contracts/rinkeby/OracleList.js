ef62ad34040df1e91570ba4285607e68e2ae929e
William <zygroup1@gmail.com>
2018-10-03 14:38:28 -0400

    AugurOracle added to Rinkeby

--- packages/bzx.js/src/contracts/rinkeby/OracleList.js
+++ packages/bzx.js/src/contracts/rinkeby/OracleList.js
@@ -2,5 +2,9 @@ export const OracleList = [
   {
     "address": "0xdcb9c38494d364781bca86870f94a713f35cf42f",
     "name": "bZxOracle"
+  },
+  {
+    "address": "0xb3841ca2b7e69d378bbe1b62a7c740a71f133fb4",
+    "name": "AugurOracle"
   }
 ];
