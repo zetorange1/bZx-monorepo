d10898409da9141e621bb4d2dc500ff639cd95b7
Alex Shafranovich <alexey.shafranovich@live.com>
2018-10-19 03:22:22 +0300

    extract addresses in started_project into separate file

---
+++ packages/starter_project/addresses.js
@@ -0,0 +1,6 @@
+module.exports = {
+  ownerAddress: "0x5409ed021d9299bf6814279a6a1411a7e866a631",   // accounts[0]
+  lenderAddress: "0xa8dda8d7f5310e4a9e24f8eba77e091ac264f872",  // accounts[5]
+  trader1Address: "0x06cef8e666768cc40cc78cf93d9611019ddcb628", // accounts[6]
+  trader2Address: "0x4404ac8bd8f9618d27ad2f1485aa1b2cfd82482d"  // accounts[7]
+};
--- packages/starter_project/scenarios/es6/all.js
+++ packages/starter_project/scenarios/es6/all.js
@@ -16,11 +16,7 @@ const { borrowOrderScenario, borrowOrderOnChainScenario } = require("./borrowOrd
 const { collateralManagementScenario } = require("./collateralManagementScenario");
 const { zeroExTradeScenario } = require("./zeroExTradeScenario");
 
-// constants from ganache-cli address 0 and 1
-const ownerAddress = "0x5409ed021d9299bf6814279a6a1411a7e866a631";    // accounts[0]
-const lenderAddress = "0xa8dda8d7f5310e4a9e24f8eba77e091ac264f872";   // accounts[5]
-const trader1Address = "0x06cef8e666768cc40cc78cf93d9611019ddcb628";  // accounts[6]
-const trader2Address = "0x4404ac8bd8f9618d27ad2f1485aa1b2cfd82482d";  // accounts[7]
+const { ownerAddress, lenderAddress, trader1Address, trader2Address } = require("./../../addresses");
 
 (async () => {
   const l = initLogger();
