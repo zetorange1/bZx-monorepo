1d369d63cc12f575bc9a06401b5bc6d89d7d3703
William <zygroup1@gmail.com>
2019-01-24 14:42:55 -0500

    update to doxity script

--- packages/contracts/extra/generate_documentation_doxity.sh
+++ packages/contracts/extra/generate_documentation_doxity.sh
@@ -1,21 +1,14 @@
 #!/bin/bash
 
 CURDIR=`/bin/pwd`
-OPENZEPPELIN="node_modules/openzeppelin-solidity"
 TRUFFLE_CONFIG_JS="truffle-config.js"
 TRUFFLE_JS="truffle.js"
-
-if [[ -L "$CURDIR/$OPENZEPPELIN" && -d "$CURDIR/$OPENZEPPELIN" ]]
-then
-    unlink $CURDIR/$OPENZEPPELIN
-fi
 
 if [[ -L "$CURDIR/$TRUFFLE_JS" && -f "$CURDIR/$TRUFFLE_JS" ]]
 then
     unlink $CURDIR/$TRUFFLE_JS
 fi
 
-ln -s $CURDIR/../../$OPENZEPPELIN $CURDIR/$OPENZEPPELIN
 ln -s $CURDIR/$TRUFFLE_CONFIG_JS $CURDIR/$TRUFFLE_JS
 
 if [ ! -f $CURDIR/scripts/doxity/pages/docs ]; then
@@ -26,5 +19,4 @@ command="$CURDIR/node_modules/.bin/doxity build"
 
 eval "$command"
 
-unlink $CURDIR/$OPENZEPPELIN
 unlink $CURDIR/$TRUFFLE_JS
