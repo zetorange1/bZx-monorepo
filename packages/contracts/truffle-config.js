d8b6ba929c4ae065aae80e035cf0338f3328fcb4
William <zygroup1@gmail.com>
2019-03-13 18:27:02 -0400

    Updates to ropsten assets

--- packages/contracts/truffle-config.js
+++ packages/contracts/truffle-config.js
@@ -59,7 +59,7 @@ module.exports = {
       ),
       network_id: 3,
       gas: 8000000,
-      gasPrice: 50000000000,
+      gasPrice: 10000000000,
       confirmations: 0,
       timeoutBlocks: 200,
       skipDryRun: true
