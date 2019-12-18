fcba0d74ebf60419768561596fef819084bf7af9
Boris Shevchenko <bshevchenkoweb@gmail.com>
2019-12-18 12:48:20 +0700

    CompoundBridge tested and fixed

--- packages/contracts/extensions/loanTokenization/contracts/Bridges/CompoundBridge.sol
+++ packages/contracts/extensions/loanTokenization/contracts/Bridges/CompoundBridge.sol
@@ -11,6 +11,7 @@ import "./BZxBridge.sol";
 interface CToken {
     function borrowBalanceCurrent(address account) external returns (uint);
     function symbol() external view returns (string memory);
+    function exchangeRateStored() external view returns (uint);
 
     function redeem(uint redeemAmount) external returns (uint);
     function transferFrom(address src, address dst, uint amount) external returns (bool);
@@ -26,13 +27,126 @@ interface CEther {
     function repayBorrowBehalf(address borrower) external payable;
 }
 
-contract CompoundBridge is BZxBridge
+contract CarefulMath {
+
+    /**
+     * @dev Possible error codes that we can return
+     */
+    enum MathError {
+        NO_ERROR,
+        DIVISION_BY_ZERO,
+        INTEGER_OVERFLOW,
+        INTEGER_UNDERFLOW
+    }
+
+    /**
+    * @dev Multiplies two numbers, returns an error on overflow.
+    */
+    function mulUInt(uint a, uint b) internal pure returns (MathError, uint) {
+        if (a == 0) {
+            return (MathError.NO_ERROR, 0);
+        }
+
+        uint c = a * b;
+
+        if (c / a != b) {
+            return (MathError.INTEGER_OVERFLOW, 0);
+        } else {
+            return (MathError.NO_ERROR, c);
+        }
+    }
+
+    /**
+    * @dev Integer division of two numbers, truncating the quotient.
+    */
+    function divUInt(uint a, uint b) internal pure returns (MathError, uint) {
+        if (b == 0) {
+            return (MathError.DIVISION_BY_ZERO, 0);
+        }
+
+        return (MathError.NO_ERROR, a / b);
+    }
+}
+
+contract Exponential is CarefulMath {
+    uint constant expScale = 1e18;
+
+    struct Exp {
+        uint mantissa;
+    }
+
+    /**
+     * @dev Creates an exponential from numerator and denominator values.
+     *      Note: Returns an error if (`num` * 10e18) > MAX_INT,
+     *            or if `denom` is zero.
+     */
+    function getExp(uint num, uint denom) pure internal returns (MathError, Exp memory) {
+        (MathError err0, uint scaledNumerator) = mulUInt(num, expScale);
+        if (err0 != MathError.NO_ERROR) {
+            return (err0, Exp({mantissa: 0}));
+        }
+
+        (MathError err1, uint rational) = divUInt(scaledNumerator, denom);
+        if (err1 != MathError.NO_ERROR) {
+            return (err1, Exp({mantissa: 0}));
+        }
+
+        return (MathError.NO_ERROR, Exp({mantissa: rational}));
+    }
+
+    /**
+     * @dev Divide a scalar by an Exp, returning a new Exp.
+     */
+    function divScalarByExp(uint scalar, Exp memory divisor) pure internal returns (MathError, Exp memory) {
+        /*
+          We are doing this as:
+          getExp(mulUInt(expScale, scalar), divisor.mantissa)
+
+          How it works:
+          Exp = a / b;
+          Scalar = s;
+          `s / (a / b)` = `b * s / a` and since for an Exp `a = mantissa, b = expScale`
+        */
+        (MathError err0, uint numerator) = mulUInt(expScale, scalar);
+        if (err0 != MathError.NO_ERROR) {
+            return (err0, Exp({mantissa: 0}));
+        }
+        return getExp(numerator, divisor.mantissa);
+    }
+
+    /**
+     * @dev Divide a scalar by an Exp, then truncate to return an unsigned integer.
+     */
+    function divScalarByExpTruncate(uint scalar, Exp memory divisor) pure internal returns (MathError, uint) {
+        (MathError err, Exp memory fraction) = divScalarByExp(scalar, divisor);
+        if (err != MathError.NO_ERROR) {
+            return (err, 0);
+        }
+
+        return (MathError.NO_ERROR, truncate(fraction));
+    }
+
+    /**
+     * @dev Truncates the given exp to a whole number value.
+     *      For example, truncate(Exp{mantissa: 15 * expScale}) = 15
+     */
+    function truncate(Exp memory exp) pure internal returns (uint) {
+        // Note: We are not using careful math here as we're performing a division that cannot fail
+        return exp.mantissa / expScale;
+    }
+}
+
+interface Comptroller {
+    function redeemAllowed(address cToken, address redeemer, uint redeemTokens) external returns (uint);
+}
+
+contract CompoundBridge is BZxBridge, Exponential // TODO clean Exponential
 {
     enum Error {
         NO_ERROR
     }
 
-    address cEther;
+    address public cEther;
     mapping(address => address) public iTokens; // cToken => iToken
     mapping(address => address) public tokens; // cToken => underlying
 
@@ -66,7 +180,7 @@ contract CompoundBridge is BZxBridge
         require(totalBorrowAmount == loanAmount, "Invalid borrow amounts value");
 
         CToken loanCToken = CToken(loanToken);
-        require(loanCToken.borrowBalanceCurrent(msg.sender) >= loanAmount);
+        require(loanCToken.borrowBalanceCurrent(msg.sender) >= loanAmount, "Invalid borrow balance");
 
         LoanTokenInterface iToken = LoanTokenInterface(iTokens[loanToken]);
 
@@ -81,6 +195,8 @@ contract CompoundBridge is BZxBridge
             )
         );
     }
+
+    function() external payable {}
 
     function _migrateLoan(
         address borrower,
@@ -111,9 +227,14 @@ contract CompoundBridge is BZxBridge
             uint amount = amounts[i];
             uint collateralAmount = collateralAmounts[i];
 
-            require(cToken.transferFrom(_borrower, address(this), amount));
+            uint cTokenAmount;
+            MathError mathErr;
+            (mathErr, cTokenAmount) = divScalarByExpTruncate(amount, Exp({mantissa: cToken.exchangeRateStored()}));
+            requireThat(mathErr == MathError.NO_ERROR, "cToken exchangeRate calc error", i);
 
-            err = cToken.redeem(amount);
+            requireThat(cToken.transferFrom(_borrower, address(this), cTokenAmount), "cToken transfer to the bridge failed", i);
+
+            err = cToken.redeem(cTokenAmount);
             requireThat(err == uint(Error.NO_ERROR), "Redeem failed", i);
 
             LoanTokenInterface iCollateral = LoanTokenInterface(iTokens[address(cToken)]);
@@ -160,13 +281,13 @@ contract CompoundBridge is BZxBridge
 
     function setCEther(address _cEther) public onlyOwner
     {
-        require(isEqual(CToken(cEther).symbol(), "cETH"), "invalid cEther address");
+        // require(isEqual(CToken(cEther).symbol(), "cETH"), "invalid cEther address");
         cEther = _cEther;
     }
 
     function setTokens(address[] memory cTokens, address[] memory _iTokens) public onlyOwner
     {
-        require(cTokens.length == _iTokens.length);
+        require(cTokens.length == _iTokens.length, "Invalid tokens");
 
         for (uint i = 0; i < cTokens.length; i++) {
             address cToken = cTokens[i];
@@ -179,15 +300,15 @@ contract CompoundBridge is BZxBridge
                     i
                 );
             } else {
-                require(isEqual(iToken.symbol(), "iETH"), "Incompatible ETH tokens");
+                // require(isEqual(iToken.symbol(), "iETH"), "Incompatible ETH tokens");
             }
             iTokens[cToken] = _iTokens[i];
             emit NewToken(cTokens[i], _iTokens[i]);
         }
     }
 
-    function isEqual(string memory a, string memory b) private pure returns (bool)
-    {
-        return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
-    }
+//    function isEqual(string memory a, string memory b) private pure returns (bool)
+//    {
+//        return (keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b)));
+//    }
 }
